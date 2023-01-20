065deee2cb1bab602d56068c3e9a3d332beae80c
marc0sfelipe <marcos12004@hotmail.com>
2023-01-20 09:48:27 -0300

    If Else

---
+++ if_else.java
@@ -0,0 +1,17 @@
+public class if_else {
+    public static void main(String[] args){
+        int nota = 30;
+        int nota_minina = 65;
+
+        if (nota >= nota_minina){
+            System.out.println("Voce passou de ano");
+        }else if (nota >= 40){
+            System.out.println("Voce ficou de recuperacao");
+        }else{
+            System.out.println("Voce reprovou");
+        }    
+
+        
+        
+    }
+}
